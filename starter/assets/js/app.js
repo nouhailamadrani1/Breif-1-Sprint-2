66d1b759c7096517962941b4e60b1d755a58806a
nouhailamadrani1 <nouhailamadrani2022@gmail.com>
2022-10-21 16:18:42 +0100

    modification

--- starter/assets/js/app.js
+++ starter/assets/js/app.js
@@ -1,14 +1,136 @@
 
-/**
- * 
- * In this file app.js you will find all CRUD functions name.
- * 
- */
-let form = document.getElementById("task")
-form.addEventListener("submit", saveTask)
 
-let feature = document.getElementById("flexRadioDefault1")
-let bug = document.getElementById("flexRadioDefault2")
+    let form = document.getElementById("task");
+    form.addEventListener("submit", saveTask);
+    var form2 = document.getElementById("taskEdit")
+    form2.addEventListener("submit", updateTask);
+
+
+    var type ;
+
+    let btnEdit = document.getElementById("edit-btn");
+    let feature = document.getElementById("flexRadioDefault1");
+    let bug = document.getElementById("flexRadioDefault2");
+
+
+    let todotasks = document.getElementById("to-do-tasks");
+    let doingtasks = document.getElementById("in-progress-tasks");
+    let donetasks = document.getElementById("done-tasks");
+
+
+        function afficher() {
+            todotasks.innerHTML = "";
+            doingtasks.innerHTML = "";
+            donetasks.innerHTML = "";
+
+         let TodoCount=0,doneCount=0,InProgressCount=0;
+         var count =0 ;
+   for (let i = 0; i < tasks.length; i++) {
+            if (tasks[i].status == "To Do") {
+            count++ ;
+        tasks[i]["id"] = count; 
+        TodoCount++;
+            todotasks.innerHTML += `
+            <button class="w-80 mb-1 rounded-3 no  border border-white mx-1  border-opacity-50 row p-2"  >
+           
+            <div class="col-1">
+              <i class="fas fa-question-circle fa-1x text-green"></i> 
+          </div>
+          <div class="col-10">
+              <div class="text-start fs-6 fw-bold ">${tasks[i]["title"]}</div>
+              <div class="">
+                  <div class="text-start  text-gray ">#${count} created in ${tasks[i]["date"]}</div>
+                  <div class="text-start" title="">${tasks[i]["description"]}</div>
+              </div>
+              <div class="d-flex">
+                  <span class="btn btn-primary btn-sm me-1 ">${tasks[i]["priority"]}</span>
+                  <span class="btn1 btn-primary btn-sm text-black bg-gray-100-800">${tasks[i]["type"]}</span>
+              </div>
+              <div class="d-flex">
+              <i class="fas fa-trash text-danger m-2" onclick="updatedelete(${count})"  ></i>
+              <i class="fas fa-pen text-green  m-2" onclick="editTask(${count})" ></i>
+
+          </div>
+          </div>
+      </button>
+        `
+            }
+            if (tasks[i].status == "In Progress") {
+                count++ ;
+                tasks[i]["id"] = count; 
+                InProgressCount++;
+            doingtasks.innerHTML += `
+            <button class="w-80 mb-1  rounded-3 no  border border-white mx-1  border-opacity-50 row p-2 " >
+								  <div class="col-1" >
+									<i class=" spinner-border spinner-border-sm text-green me-1 " role="status "></i> 
+								</div>
+								<div class="col-10">
+									<div class="text-start fs-6 fw-bold ">${tasks[i]["title"]}</div>
+									<div class="">
+										<div class="text-start  text-gray ">#${count} created in ${tasks[i]["date"]}</div>
+										<div class="text-start" title="">${tasks[i]["description"]}</div>
+									</div>
+									<div class="d-flex">
+										<span class="btn btn-primary btn-sm me-1 ">${tasks[i]["priority"]}</span>
+										<span class="btn1 btn-primary btn-sm text-black bg-gray-100-800">${tasks[i]["type"]}</span>
+									</div>
+                                    <div class="d-flex">
+                                    <i class="fas fa-trash text-danger m-2" onclick="updatedelete(${count})"  ></i>
+                                    <i class="fas fa-pen text-green  m-2" onclick="editTask(${count})" ></i>
+									</div>
+								</div>
+							</button>`
+            }
+            
+            if (tasks[i].status == "Done") {
+                count++;
+                tasks[i]["id"] = count; 
+                doneCount++;
+            donetasks.innerHTML += `   
+        <button class="w-80 mb-1 rounded-3 no  border border-white mx-1  border-opacity-50 row p-2" >
+        <div class="col-1">
+          <i class="fas fa-chevron-circle-down fa-1x text-green p-2 "></i> 
+      </div>
+      <div class="col-10">
+          <div class="text-start fs-6 fw-bold ">${tasks[i]["title"]}</div>
+          <div class="col-10">
+              <div class="text-start  text-gray ">#${count} created in ${tasks[i]["date"]}</div>
+              <div class="text-start" title="">${tasks[i]["description"]}</div>
+          </div>
+          <div class="d-flex">
+              <span class="btn btn-primary btn-sm me-1 ">${tasks[i]["priority"]}</span>
+              <span class="btn1 btn-primary btn-sm text-black bg-gray-100-800">${tasks[i]["type"]}</span>
+          </div>
+          <div class="d-flex">
+          <i class="fas fa-trash text-danger m-2" onclick="updatedelete(${count})"  ></i>
+          <i class="fas fa-pen text-green  m-2" onclick="editTask(${count})" ></i>
+      </div>
+      </div>
+  </button>
+        `
+        }
+        if (tasks[i].status == "Done") {
+            doingtasks.innerHTML += ``
+        }
+    }
+
+    document.getElementById('to-do-tasks-count').innerHTML=`${TodoCount}`;
+    document.getElementById('in-progress-tasks-count').innerHTML=`${InProgressCount}`;
+    document.getElementById('done-tasks-count').innerHTML=`${doneCount}`;
+    
+}
+afficher();
+function updatedelete(id)
+{
+    for(i=0; i<tasks.length ; i++){
+        if(id== tasks[i].id)
+        {
+            tasks.splice(id-1,1);
+            afficher();
+        }
+    }
+    
+}
 function createTask() {
         
     // document.getElementById("send").addEventListener("click", )
@@ -21,8 +143,8 @@ function createTask() {
 
 function saveTask(e) {
     // stop reload
-    e.preventDefault()
-    let type
+    e.preventDefault();
+    let type;
     if(feature.checked) {
         type = "feature"
     } else {
@@ -38,35 +160,74 @@ function saveTask(e) {
         description:form.description.value   
     }
 
-    tasks.push(task)
-    console.log(tasks);
-    afficher()
+    tasks.push(task);
+    // console.log(tasks);
+    Swal.fire(
+        'good job',
+        'you successfully saved your task',
+        'success'
+      )
+      $('#task').modal('hide');
+    afficher();
+   
+    
 }
 
-function editTask(index) {
-    // Initialisez task form
+function editTask(id)
+ {
 
-    // Affichez updates
-
-    // Delete Button
-
-    // Définir l’index en entrée cachée pour l’utiliser en Update et Delete
-
-    // Definir FORM INPUTS
-
-    // Ouvrir Modal form
+    $('#edit').modal('show');
+    for(i=0; i<tasks.length ; i++)
+    {
+               if(id== tasks[i].id)
+               {
+                // let type ;
+                if(tasks[i].type =="feature") {
+                   formEdit.type.checked = true;
+                } else {
+                   type = "bug"
+                }
+                
+                document.getElementById('title').value =tasks[i].title;
+                document.getElementById('status').value=tasks[i].status;
+                document.getElementById('priority').value=tasks[i].priority;
+                document.getElementById('date').value=tasks[i].date;
+                document.getElementById('description').value =tasks[i].description;
+                    
+               }
+    }
+            btnEdit.innerHTML=`<button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="reset">Close</button>
+            <button type="submit" class="btn btn-primary" onclick="updateTask(${id})">Save edit changes</button>`;
+           
+            // console.log(btnEdit);
+            
 }
 
-function updateTask() {
+function updateTask(i) {
     // GET TASK ATTRIBUTES FROM INPUTS
 
+console.log("hiiii");
     // Créez task object
-
+    console.log(tasks[i]);
+    let task = {
+        id :i,
+        title: document.getElementById('title').value,
+        type: type,
+        status:document.getElementById('status').value,
+        priority:document.getElementById('priority').value,
+        date:document.getElementById('date').value,
+        description:document.getElementById('description').value
+    }
+    console.log(task);
     // Remplacer ancienne task par nouvelle task
-
+    tasks[i-1]=task;
+    
     // Fermer Modal form
-
+    $('#edit').modal('hide');
     // Refresh tasks
+    
+    afficher();
+    
     
 }
 
@@ -91,99 +252,5 @@ function reloadTasks() {
 
     // Set Task count
 }
-let todotasks = document.getElementById("to-do-tasks");
- let doingtasks = document.getElementById("in-progress-tasks");
- let donetasks = document.getElementById("done-tasks");
- afficher()
- console.log(tasks);
- function afficher() {
-   todotasks.innerHTML = "";
-   doingtasks.innerHTML = "";
-   donetasks.innerHTML = "";
-   var count =0 ;
-   for (let i = 0; i < tasks.length; i++) {
-            if (tasks[i].status == "To Do") {
- count++ ;
-tasks[i]["id"] = count; 
-            todotasks.innerHTML += `
-            <button class="w-80 mb-1 rounded-3 no  border border-white mx-1  border-opacity-50 row p-2" onclick="updatedelete(${count})">
-            <div class="col-1">
-              <i class="fas fa-question-circle fa-1x text-green"></i> 
-          </div>
-          <div class="col-10">
-              <div class="text-start fs-6 fw-bold ">${tasks[i]["title"]}</div>
-              <div class="">
-                  <div class="text-start  text-gray ">#${count} created in ${tasks[i]["date"]}</div>
-                  <div class="text-start" title="">${tasks[i]["description"]}</div>
-              </div>
-              <div class="d-flex">
-                  <span class="btn btn-primary btn-sm me-1 ">${tasks[i]["priority"]}</span>
-                  <span class="btn1 btn-primary btn-sm text-black bg-gray-100-800">${tasks[i]["type"]}</span>
-              </div>
-          </div>
-      </button>
-        `
-            }
-            if (tasks[i].status == "In Progress") {
-                count++ ;
-                tasks[i]["id"] = count; 
-            doingtasks.innerHTML += `
-            <button class="w-80 mb-1  rounded-3 no  border border-white mx-1  border-opacity-50 row p-2 " onclick="updatedelete(${count})">
-								  <div class="col-1" >
-									<i class=" spinner-border spinner-border-sm text-green me-1 " role="status "></i> 
-								</div>
-								<div class="col-10">
-									<div class="text-start fs-6 fw-bold ">${tasks[i]["title"]}</div>
-									<div class="">
-										<div class="text-start  text-gray ">#${count} created in ${tasks[i]["date"]}</div>
-										<div class="text-start" title="">${tasks[i]["description"]}</div>
-									</div>
-									<div class="d-flex">
-										<span class="btn btn-primary btn-sm me-1 ">${tasks[i]["priority"]}</span>
-										<span class="btn1 btn-primary btn-sm text-black bg-gray-100-800">${tasks[i]["type"]}</span>
-									</div>
-								</div>
-							</button>`
-            }
-            
-            if (tasks[i].status == "Done") {
-                count++;
-                tasks[i]["id"] = count; 
-            donetasks.innerHTML += `
-                        
-            
-        
-        <button class="w-80 mb-1 rounded-3 no  border border-white mx-1  border-opacity-50 row p-2" onclick="updatedelete(${count})">
-        <div class="col-1">
-          <i class="fas fa-chevron-circle-down fa-1x text-green p-2 "></i> 
-      </div>
-      <div class="col-10">
-          <div class="text-start fs-6 fw-bold ">${tasks[i]["title"]}</div>
-          <div class="col-10">
-              <div class="text-start  text-gray ">#${count} created in ${tasks[i]["date"]}</div>
-              <div class="text-start" title="">${tasks[i]["description"]}</div>
-          </div>
-          <div class="d-flex">
-              <span class="btn btn-primary btn-sm me-1 ">${tasks[i]["priority"]}</span>
-              <span class="btn1 btn-primary btn-sm text-black bg-gray-100-800">${tasks[i]["type"]}</span>
-          </div>
-      </div>
-  </button>
-        `
-        }
-        if (tasks[i].status == "Done") {
-            doingtasks.innerHTML += ``
-        }
-    }
-}
-function updatedelete(id)
-{
-    for(i=0; i<tasks.length ; i++){
-        if(id== tasks[i].id)
-        {
-            tasks.splice(id-1,1);
-            afficher();
-        }
-    }
-    
-}
+
+
--- starter/assets/js/data.js
+++ starter/assets/js/data.js
@@ -2,46 +2,46 @@
     This file contains an array of objects as tasks, to be loaded in the execution.
 */
 var tasks = [
-    {
-        'id ': '',
-        'title'         :   'Keep all the updated requirements in one place',
-        'type'          :   'Feature',
-        'priority'      :   'High',
-        'status'        :   'To Do',
-        'date'          :   '2022-10-08',
-        'description'   :   `There is hardly anything more frustrating than having to look for current requirements in tens of comments under the actual description or having to decide which commenter is actually authorized to change the requirements. The goal here is to keep all the up-to-date requirements and details in the main/primary description of a task. Even though the information in comments may affect initial criteria, just update this primary description accordingly.`,
-    },
-    {
-        'id ': '',
-        'title'         :   'Consider creating an acceptance criteria list',
-        'type'          :   'Feature',
-        'priority'      :   'High',
-        'status'        :   'To Do',
-        'date'          :   '2022-10-08',
-        'description'   :   `Descriptive requirements are very helpful when it comes to understanding the context of a problem, yet finally it is good to precisely specify what is expected. Thus the developer will not have to look for the actual requirements in a long, descriptive text but he will be able to easily get to the essence. One might find that sometimes — when acceptance criteria are well defined — there is little or no need for any additional information. Example:
-        a) User navigates to “/accounts” and clicks on red download CSV button
-        b) Popup appears with two buttons: “This year” and “Last year”
-        c) If user clicked on “Last year” download is initiated
-        d) CSV downloaded includes following columns…`,
-    },
-    {
-        'id ': '',
-        'title'         :   'Provide mockups',
-        'type'          :   'Feature',
-        'priority'      :   'High',
-        'status'        :   'To Do',
-        'date'          :   '2022-10-08',
-        'description'   :   `A textual requirements description is essential in most cases, but an image is often worth more than a thousand words. Even a simple mockup can limit misunderstandings by a great factor. There are many apps out there that might be helpful here, like Balsamiq, InVision or Mockingbird, but manipulating screenshots of an existing app also works.`,
-    },
-    {
-        'id ': '',
-        'title'         :   'Provide examples, credentials, etc',
-        'type'          :   'Feature',
-        'priority'      :   'High',
-        'status'        :   'To Do',
-        'date'          :   '2022-10-08',
-        'description'   :   `If the expectation is to process or generate some file — attach an example of such a file. If the goal is to integrate what is being developed with some service, ensure your devs have access to this service and its documentation. This list could go on and on — the bottom line is — if there is something that our developer might make use of, try to foresee it and provide them with (access to) it.`,
-    },
+    // {
+    //     'id ': '',
+    //     'title'         :   'Keep all the updated requirements in one place',
+    //     'type'          :   'Feature',
+    //     'priority'      :   'High',
+    //     'status'        :   'To Do',
+    //     'date'          :   '2022-10-08',
+    //     'description'   :   `There is hardly anything more frustrating than having to look for current requirements in tens of comments under the actual description or having to decide which commenter is actually authorized to change the requirements. The goal here is to keep all the up-to-date requirements and details in the main/primary description of a task. Even though the information in comments may affect initial criteria, just update this primary description accordingly.`,
+    // },
+    // {
+    //     'id ': '',
+    //     'title'         :   'Consider creating an acceptance criteria list',
+    //     'type'          :   'Feature',
+    //     'priority'      :   'High',
+    //     'status'        :   'To Do',
+    //     'date'          :   '2022-10-08',
+    //     'description'   :   `Descriptive requirements are very helpful when it comes to understanding the context of a problem, yet finally it is good to precisely specify what is expected. Thus the developer will not have to look for the actual requirements in a long, descriptive text but he will be able to easily get to the essence. One might find that sometimes — when acceptance criteria are well defined — there is little or no need for any additional information. Example:
+    //     a) User navigates to “/accounts” and clicks on red download CSV button
+    //     b) Popup appears with two buttons: “This year” and “Last year”
+    //     c) If user clicked on “Last year” download is initiated
+    //     d) CSV downloaded includes following columns…`,
+    // },
+    // {
+    //     'id ': '',
+    //     'title'         :   'Provide mockups',
+    //     'type'          :   'Feature',
+    //     'priority'      :   'High',
+    //     'status'        :   'To Do',
+    //     'date'          :   '2022-10-08',
+    //     'description'   :   `A textual requirements description is essential in most cases, but an image is often worth more than a thousand words. Even a simple mockup can limit misunderstandings by a great factor. There are many apps out there that might be helpful here, like Balsamiq, InVision or Mockingbird, but manipulating screenshots of an existing app also works.`,
+    // },
+    // {
+    //     'id ': '',
+    //     'title'         :   'Provide examples, credentials, etc',
+    //     'type'          :   'Feature',
+    //     'priority'      :   'High',
+    //     'status'        :   'To Do',
+    //     'date'          :   '2022-10-08',
+    //     'description'   :   `If the expectation is to process or generate some file — attach an example of such a file. If the goal is to integrate what is being developed with some service, ensure your devs have access to this service and its documentation. This list could go on and on — the bottom line is — if there is something that our developer might make use of, try to foresee it and provide them with (access to) it.`,
+    // },
     {
         'id ': '',
         'title'         :   'Annotate',
@@ -69,24 +69,24 @@ var tasks = [
         'date'          :   '2022-10-08',
         'description'   :   `It is always safer to assume less rather than more domain knowledge in the dev team. Therefore following the KISS principle and augmenting each description or acceptance criteria list with contextual/domain knowledge and details that might become relevant is highly recommended.`,
     },
-    {
-        'id ': '',
-        'title'         :   'Describe edge cases and provide constraints',
-        'type'          :   'Feature',
-        'priority'      :   'High',
-        'status'        :   'In Progress',
-        'date'          :   '2022-10-08',
-        'description'   :   `Hardly any developer likes constraints, but if there are some, let them be communicated early. Do we need to support some specific browsers? Does this script need to run below a specific amount of time? Is it crucial for this endpoint to respond in no more than n milliseconds? If there are some such concerns, make sure they are included in your descriptions. Also describing any edge cases might be beneficial. Maybe we have some query limit on a given service? If you have such knowledge it is always beneficial for your devs to know about it upfront.`,
-    },
-    {
-        'id ': '',
-        'title'         :   'Provide a copy',
-        'type'          :   'Feature',
-        'priority'      :   'High',
-        'status'        :   'In Progress',
-        'date'          :   '2022-10-08',
-        'description'   :   `If there is a long message to be displayed, just provide a copy for it somewhere in the description. Do not place it on mockups as it is always slower and more error-prone to re-type it than to copy-paste it.`,
-    },
+    // {
+    //     'id ': '',
+    //     'title'         :   'Describe edge cases and provide constraints',
+    //     'type'          :   'Feature',
+    //     'priority'      :   'High',
+    //     'status'        :   'In Progress',
+    //     'date'          :   '2022-10-08',
+    //     'description'   :   `Hardly any developer likes constraints, but if there are some, let them be communicated early. Do we need to support some specific browsers? Does this script need to run below a specific amount of time? Is it crucial for this endpoint to respond in no more than n milliseconds? If there are some such concerns, make sure they are included in your descriptions. Also describing any edge cases might be beneficial. Maybe we have some query limit on a given service? If you have such knowledge it is always beneficial for your devs to know about it upfront.`,
+    // },
+    // {
+    //     'id ': '',
+    //     'title'         :   'Provide a copy',
+    //     'type'          :   'Feature',
+    //     'priority'      :   'High',
+    //     'status'        :   'In Progress',
+    //     'date'          :   '2022-10-08',
+    //     'description'   :   `If there is a long message to be displayed, just provide a copy for it somewhere in the description. Do not place it on mockups as it is always slower and more error-prone to re-type it than to copy-paste it.`,
+    // },
     
     {
         'id ': '',
@@ -115,24 +115,24 @@ var tasks = [
         'date'          :   '2022-10-08',
         'description'   :   `i.e., browser version, operating system version etc. Sometimes a list of installed browser plugins and extensions might be helpful as well.`,
     },
-    {
-        'id ': '',
-        'title'         :   'Provide a link to an exception and/or a stack trace',
-        'type'          :   'Bug',
-        'priority'      :   'High',
-        'status'        :   'In Progress',
-        'date'          :   '2022-10-08',
-        'description'   :   `as investigating those is usually the first step to take in resolving the problem.`,
-    },
-    {
-        'id ': '',
-        'title'         :   'Provide access to logs',
-        'type'          :   'Bug',
-        'priority'      :   'High',
-        'status'        :   'Done',
-        'date'          :   '2022-10-08',
-        'description'   :   `as they can be helpful in reproducing the steps that caused the problem in the first place.`,
-    },
+    // {
+    //     'id ': '',
+    //     'title'         :   'Provide a link to an exception and/or a stack trace',
+    //     'type'          :   'Bug',
+    //     'priority'      :   'High',
+    //     'status'        :   'In Progress',
+    //     'date'          :   '2022-10-08',
+    //     'description'   :   `as investigating those is usually the first step to take in resolving the problem.`,
+    // },
+    // {
+    //     'id ': '',
+    //     'title'         :   'Provide access to logs',
+    //     'type'          :   'Bug',
+    //     'priority'      :   'High',
+    //     'status'        :   'Done',
+    //     'date'          :   '2022-10-08',
+    //     'description'   :   `as they can be helpful in reproducing the steps that caused the problem in the first place.`,
+    // },
     {
         'id ': '',
         'title'         :   'Provide access to the affected server or database dump',
